a1844502b4ecba0c578a5c265417efa9e0756888
david <danaya@gmail.com>
2017-06-05 08:14:12 +0200

    all but auth

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyAkE4Km7aevqlSwVkM5BGYskjmjw_umYbc",
+  authDomain: "catch-of-the-day-david-anaya.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-david-anaya.firebaseio.com",
+});
+
+export default base;
